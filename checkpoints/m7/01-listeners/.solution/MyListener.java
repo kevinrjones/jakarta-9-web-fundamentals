346dd6d6af67ebd77437db7f0836b250dbffbb16
Kevin Jones <kevin@knowledgespike.com>
2022-09-28 20:03:58 +0100

    Updated code

--- checkpoints/m7/01-listeners/.solution/MyListener.java
+++ checkpoints/m7/01-listeners/.solution/MyListener.java
@@ -6,6 +6,6 @@ import jakarta.servlet.ServletContextListener;
 public class MyListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ServletContextListener.super.contextInitialized(sce);
+
     }
 }

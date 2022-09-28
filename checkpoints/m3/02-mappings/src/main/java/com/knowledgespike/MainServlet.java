c646dd86948dfda52d109224cbbc692f0527460a
Kevin Jones <kevin@knowledgespike.com>
2022-09-28 16:58:00 +0100

    Updated code for the checkpoint

--- checkpoints/m3/02-mappings/src/main/java/com/knowledgespike/MainServlet.java
+++ checkpoints/m3/02-mappings/src/main/java/com/knowledgespike/MainServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"*.do", "/home"})
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
